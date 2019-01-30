0efe4b4f76ac3c5b2fa062dac455f8d8259dadd4
descomkraneoz <36744638+descomkraneoz@users.noreply.github.com>
2019-01-30 09:20:42 +0100

    Add files via upload
    Terminado

--- DepositoCombustible/testMMS/DepositoCombustibleTest.java
+++ DepositoCombustible/testMMS/DepositoCombustibleTest.java
@@ -7,10 +7,11 @@ public class DepositoCombustibleTest {
 	
 	private DepositoCombustible miDeposito;
 	private DepositoCombustible otroDeposito=new DepositoCombustible(100.0,75.5);
-	
+	private	double amount;
+
 	@Before
 	public void antesQue() {
-		miDeposito= new DepositoCombustible(100.0,0.0);
+		miDeposito= new DepositoCombustible(100.0,20.0);
 	}	
 	
 	@Test
@@ -25,25 +26,44 @@ public class DepositoCombustibleTest {
 
 	@Test
 	public void testEstaVacio() {
-		assertTrue(miDeposito.estaVacio());
-		assertFalse(miDeposito.estaLleno());
-
+		assertFalse(miDeposito.estaVacio());
 	}
 
 	@Test
 	public void testEstaLleno() {
-		assertFalse(otroDeposito.estaLleno());
-		assertFalse(otroDeposito.estaVacio());
+		assertFalse(miDeposito.estaLleno());
 	}
 
 	@Test
-	public void testFill() {
-		fail("Not yet implemented");
+	public void testFill() {		
+		System.out.println("Capacidad del deposito: "+miDeposito.getDepositoMax());
+		System.out.println("Liquido del deposito: "+miDeposito.getDepositoNivel());
+		amount=30.0;
+		miDeposito.fill(amount);
+		System.out.println("Despues del llenado");
+		System.out.println("Capacidad del deposito: "+miDeposito.getDepositoMax());
+		System.out.println("Liquido del deposito: "+miDeposito.getDepositoNivel());
+		double depNivelInicial=miDeposito.getDepositoNivel()-amount;		
+		double depNivelFinal=miDeposito.getDepositoNivel();
+		assertEquals(depNivelFinal,amount+depNivelInicial,0);
+		
 	}
 
 	@Test
 	public void testConsumir() {
-		fail("Not yet implemented");
+		System.out.println("Nivel de liquido del deposito al inicio: "+miDeposito.getDepositoNivel());
+		amount=10.0;
+		miDeposito.consumir(amount);
+		System.out.println("Nivel de liquido del deposito al final: "+miDeposito.getDepositoNivel());
+		double depNivelInicial=miDeposito.getDepositoNivel()+amount;		
+		double depNivelFinal=miDeposito.getDepositoNivel();
+		assertEquals(depNivelFinal,depNivelInicial-amount,0);
+		
+	}
+	
+	@Test
+	public void testNoSonIguales() {
+		assertNotSame(miDeposito,otroDeposito);		
 	}
 
 }
